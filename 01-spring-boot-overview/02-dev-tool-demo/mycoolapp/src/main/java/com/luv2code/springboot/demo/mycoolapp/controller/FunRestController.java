0fde8dbe3078422fb0e8b5e26527b6ecf6523deb
josefortyfive <jose.fortyfive@gmail.com>
2024-08-15 11:27:03 +0800

    Added new function in FunRestController.java

--- 01-spring-boot-overview/02-dev-tool-demo/mycoolapp/src/main/java/com/luv2code/springboot/demo/mycoolapp/controller/FunRestController.java
+++ 01-spring-boot-overview/02-dev-tool-demo/mycoolapp/src/main/java/com/luv2code/springboot/demo/mycoolapp/controller/FunRestController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FunRestController {
 
+    // expose
     @GetMapping("/")
     public String returnHelloWorld(){
         return "Hello World!";
@@ -15,4 +16,19 @@ public class FunRestController {
     public String returnHelloWorld2(){
         return "this is Hello World2";
     }
+
+    //expose a new endpoint for "workout"
+
+    @GetMapping("/workout")
+    public String getDailyWorkout(){
+        return "Run a hard 5k";
+    }
+
+    //expose a new endpoint for "fortune"
+
+    @GetMapping("/fortune")
+    public String getDailyFortune(){
+        return "Today is your lucky day!";
+    }
+
 }
