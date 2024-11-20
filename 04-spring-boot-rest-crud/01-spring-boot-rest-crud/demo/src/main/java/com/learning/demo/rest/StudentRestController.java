c7c432aa1398b209cba3a1a9c952024b62ff4757
josefortyfive <jose.fortyfive@gmail.com>
2024-11-20 15:12:17 +0800

    Added loadData() function

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/demo/src/main/java/com/learning/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/demo/src/main/java/com/learning/demo/rest/StudentRestController.java
@@ -1,6 +1,7 @@
 package com.learning.demo.rest;
 
 import com.learning.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,14 +13,23 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    @GetMapping("/students")
-    public List<Student> getStudents(){
+    private List<Student> students;
 
-        List<Student> students = new ArrayList<>();
+    // define @PostConstruct to load the student data ... only once!
+
+    @PostConstruct
+    public void loadData(){
+
+        students = new ArrayList<>();
+
         students.add(new Student("Maria", "Elena"));
         students.add(new Student("James", "Mark"));
         students.add(new Student("Mike", "Jordan"));
 
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents(){
         return students;
     }
 
