887d57ffa8546abbbf1e38f70269ab6c7309b145
josefortyfive <jose.fortyfive@gmail.com>
2024-11-20 15:31:23 +0800

    added getStudent function

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/demo/src/main/java/com/learning/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/demo/src/main/java/com/learning/demo/rest/StudentRestController.java
@@ -3,6 +3,7 @@ package com.learning.demo.rest;
 import com.learning.demo.entity.Student;
 import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,6 +32,15 @@ public class StudentRestController {
     @GetMapping("/students")
     public List<Student> getStudents(){
         return students;
+    }
+
+    // define endpoint or "/student/{studentId}" - return student at index
+
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId){
+        
+        return students.get(studentId);
     }
 
 }
