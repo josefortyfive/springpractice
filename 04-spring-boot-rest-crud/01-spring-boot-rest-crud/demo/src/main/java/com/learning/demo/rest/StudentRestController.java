dcae5aa5bb637b2711b7f5665865cd0ba5b424d1
josefortyfive <jose.fortyfive@gmail.com>
2024-11-22 16:23:43 +0800

    Added another customer exception

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/demo/src/main/java/com/learning/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/demo/src/main/java/com/learning/demo/rest/StudentRestController.java
@@ -59,4 +59,21 @@ public class StudentRestController {
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 
+    // Add another exception hander ... to catch any exception (catch all)
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(Exception exc){
+
+        // create a StudentErrorResponse
+
+        StudentErrorResponse error = new StudentErrorResponse();
+
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        // error.setMessage("Not a valid input"); -- This is of an custom input
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+
+    }
 }
