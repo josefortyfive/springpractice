919c24849ac33726ee4b642b500f8aabe591c687
josefortyfive <jose.fortyfive@gmail.com>
2024-11-26 15:02:37 +0800

    Added Rest for Employee

---
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/cruddemo/src/main/java/com/learning/springboot/cruddemo/rest/EmployeeRestController.java
@@ -0,0 +1,26 @@
+package com.learning.springboot.cruddemo.rest;
+
+import com.learning.springboot.cruddemo.dao.EmployeeDAO;
+import com.learning.springboot.cruddemo.entity.Employee;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+
+    public EmployeeDAO employeeDAO;
+
+    public EmployeeRestController(EmployeeDAO employeeDAO){
+        this.employeeDAO = employeeDAO;
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> findAll(){
+        return employeeDAO.findAll();
+    }
+
+}
