3f7b2c079adeb91c1db7f152d463a0de2f2cf1e4
josefortyfive <jose.fortyfive@gmail.com>
2024-11-27 09:22:05 +0800

    Implemented the findById function

--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/cruddemo/src/main/java/com/learning/springboot/cruddemo/rest/EmployeeRestController.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/cruddemo/src/main/java/com/learning/springboot/cruddemo/rest/EmployeeRestController.java
@@ -4,6 +4,7 @@ import com.learning.springboot.cruddemo.dao.EmployeeDAO;
 import com.learning.springboot.cruddemo.entity.Employee;
 import com.learning.springboot.cruddemo.service.EmployeeService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,4 +25,14 @@ public class EmployeeRestController {
         return employeeService.findAll();
     }
 
+    @GetMapping("/employees/{employeeId}")
+    public Employee findById(@PathVariable int employeeId){
+        Employee employee = employeeService.findById(employeeId);
+
+        if(employee == null){
+            throw new RuntimeException("Employee id not found " +employeeId);
+        }
+
+        return employee;
+    }
 }
