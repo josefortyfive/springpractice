ff58c93a137072fd87aab81039f8a73038d8ad7f
josefortyfive <jose.fortyfive@gmail.com>
2024-11-27 09:35:00 +0800

    Implemented save function

--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/cruddemo/src/main/java/com/learning/springboot/cruddemo/rest/EmployeeRestController.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/cruddemo/src/main/java/com/learning/springboot/cruddemo/rest/EmployeeRestController.java
@@ -3,10 +3,7 @@ package com.learning.springboot.cruddemo.rest;
 import com.learning.springboot.cruddemo.dao.EmployeeDAO;
 import com.learning.springboot.cruddemo.entity.Employee;
 import com.learning.springboot.cruddemo.service.EmployeeService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -34,5 +31,18 @@ public class EmployeeRestController {
         }
 
         return employee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee employee){
+
+        // also just incase they pass an id in JSON .... set id to 0
+        // this is to force a save of new item ... instead of update
+
+        employee.setId(0);
+        Employee dbEmployee = employeeService.save(employee);
+        return dbEmployee;
+
+
     }
 }
