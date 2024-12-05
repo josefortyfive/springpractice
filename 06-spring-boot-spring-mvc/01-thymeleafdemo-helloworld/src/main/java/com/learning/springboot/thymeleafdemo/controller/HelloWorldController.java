39a6255fb80287b48214cb798920f5d15dc8a6c6
josefortyfive <jose.fortyfive@gmail.com>
2024-12-05 09:44:32 +0800

    Added processFormVersionTwo

--- 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/learning/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/learning/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -1,6 +1,8 @@
 package com.learning.springboot.thymeleafdemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,12 +10,36 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HelloWorldController {
     // new a controller method to show initial HTML form
 
-    @RequestMapping("/showform")
+    @RequestMapping("/showForm")
     public String showForm(){
         return "helloworld-form";
     }
 
     // need a controller method to process the form
-    
+    @RequestMapping("/processForm")
+    public String processForm(){
+        return "helloworld";
+    }
+
+    // need a controller method to read form data and
+    // add data to the model
+
+    @RequestMapping("/processFormVersionTwo")
+    public String letShoutDude(HttpServletRequest request, Model model){
+
+        // read the request parameter from the HTML form
+        String name = request.getParameter("studentName");
+
+        // convert the data to all caps
+        name = name.toUpperCase();
+
+        // create the message
+        String result = "Yo! " +name;
+
+        // add message to the model
+        model.addAttribute("message", result);
+
+        return "helloworld";
+    }
 
 }
