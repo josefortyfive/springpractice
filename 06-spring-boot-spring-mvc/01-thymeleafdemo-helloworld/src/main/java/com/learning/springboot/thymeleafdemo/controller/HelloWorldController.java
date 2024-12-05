5c0d48359afc335b53ec2c0a2644ddf0cf9c55ab
josefortyfive <jose.fortyfive@gmail.com>
2024-12-05 14:02:08 +0800

    Added processFormVersionThree() function

--- 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/learning/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/learning/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -42,4 +43,19 @@ public class HelloWorldController {
         return "helloworld";
     }
 
+
+    @RequestMapping("/processFormVersionThree")
+    public String processFormVersionThree(@RequestParam("studentName") String name, Model model){
+
+        // convert the data to all caps
+        name = name.toUpperCase();
+
+        // create the message
+        String result = "Hi There, " +name+ " !";
+
+        // add message to the model
+        model.addAttribute("message", result);
+
+        return "helloworld";
+    }
 }
