187af474b628c269e459a3a80dbca930f25b80e0
josefortyfive <jose.fortyfive@gmail.com>
2024-12-06 16:16:46 +0800

    changed the function Annotation of showForm() to @GetMapping

--- 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/learning/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/learning/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class HelloWorldController {
     // new a controller method to show initial HTML form
 
-    @RequestMapping("/showForm")
+    @GetMapping("/showForm")
     public String showForm(){
         return "helloworld-form";
     }
