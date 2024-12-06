84a24d3df06a5cbc37b720003ba19a7cff450cd6
josefortyfive <jose.fortyfive@gmail.com>
2024-12-06 16:54:29 +0800

    added student-form.html

--- 06-spring-boot-spring-mvc/02-thymeleafdemo-student/src/main/java/com/learning/springboot/thymeleafdemo/controller/StudentController.java
+++ 06-spring-boot-spring-mvc/02-thymeleafdemo-student/src/main/java/com/learning/springboot/thymeleafdemo/controller/StudentController.java
@@ -4,6 +4,8 @@ import com.learning.springboot.thymeleafdemo.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class StudentController {
@@ -18,5 +20,13 @@ public class StudentController {
         model.addAttribute("student", student);
 
         return "student-form";
+    }
+
+    @PostMapping("/processStudentForm")
+    public String processStudentForm(@ModelAttribute("student") Student student){
+
+        System.out.println("Student: " +student.getFirstName()+ " " +student.getLastName());
+        
+        return "student-confirmation";
     }
 }
