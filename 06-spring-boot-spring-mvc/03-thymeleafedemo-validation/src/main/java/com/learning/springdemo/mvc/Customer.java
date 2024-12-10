621dbbdbdf240e3da075e2ef19334018720fe07c
josefortyfive <jose.fortyfive@gmail.com>
2024-12-10 16:35:02 +0800

    Added postalCode with regex @Pattern

--- 06-spring-boot-spring-mvc/03-thymeleafedemo-validation/src/main/java/com/learning/springdemo/mvc/Customer.java
+++ 06-spring-boot-spring-mvc/03-thymeleafedemo-validation/src/main/java/com/learning/springdemo/mvc/Customer.java
@@ -1,9 +1,6 @@
 package com.learning.springdemo.mvc;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public class Customer {
 
@@ -16,6 +13,9 @@ public class Customer {
     @Min(value=0, message="must be greater than or equal to zero")
     @Max(value=10, message="must be less than or equal to 10")
     private int freePass;
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
+    private String postalCode;
 
     public String getFirstName() {
         return firstName;
@@ -41,5 +41,13 @@ public class Customer {
 
     public void setFreePass(@Min(value = 0, message = "must be greater than or equal to zero") @Max(value = 10, message = "must be less than or equal to 10") int freePass) {
         this.freePass = freePass;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
     }
 }
