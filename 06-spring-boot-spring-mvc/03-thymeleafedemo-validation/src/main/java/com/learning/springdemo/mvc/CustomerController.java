792e0e17e1a0553fc93177dc52b78e66394ae3f8
josefortyfive <jose.fortyfive@gmail.com>
2024-12-11 08:12:39 +0800

    Created CourseCode.java for custom annotation

--- 06-spring-boot-spring-mvc/03-thymeleafedemo-validation/src/main/java/com/learning/springdemo/mvc/CustomerController.java
+++ 06-spring-boot-spring-mvc/03-thymeleafedemo-validation/src/main/java/com/learning/springdemo/mvc/CustomerController.java
@@ -37,6 +37,9 @@ public class CustomerController {
 
         System.out.println("Last Name: | "+theCustomer.getLastName()+ " | ");
 
+        System.out.println("Binding results: " +theBindingResult.toString());
+
+        System.out.println("\n\n\n\n");
         if(theBindingResult.hasErrors()){
             return "customer-form";
         }
