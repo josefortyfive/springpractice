8bb704215249ae83e1bbc29f8f9b1167692cd028
josefortyfive <jose.fortyfive@gmail.com>
2024-12-11 15:54:37 +0800

    added EmployeeController

---
+++ 07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeeController.java
@@ -0,0 +1,35 @@
+package com.luv2code.springboot.cruddemo.controller;
+
+import com.luv2code.springboot.cruddemo.entity.Employee;
+import com.luv2code.springboot.cruddemo.service.EmployeeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService){
+        this.employeeService = employeeService;
+    }
+
+    // add mapping for "/list"
+
+    @GetMapping("/list")
+    public String listEmployees(Model model){
+
+        // get the employee from db
+        List<Employee> employeeList = employeeService.findAll();
+
+        // add to the spring model
+        model.addAttribute("emplyees", employeeList);
+
+        return "list-employees";
+    }
+}
