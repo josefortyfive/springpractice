33f573eb334f168d07ec9f043bd8befdc46ee115
josefortyfive <jose.fortyfive@gmail.com>
2024-12-15 19:13:53 +0800

    Added role for ADMIN

--- 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/learning/sprigboot/demosecurity/controller/DemoController.java
+++ 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/learning/sprigboot/demosecurity/controller/DemoController.java
@@ -12,11 +12,15 @@ public class DemoController {
     }
 
     // add a request mapping for /leaders
-
     @GetMapping("/leaders")
     public String showLeaders() {
         return "leaders";
     }
 
+    // add request mapping for /systems
+    @GetMapping("/systems")
+    public String showSystems() {
+        return "systems";
+    }
 
 }
