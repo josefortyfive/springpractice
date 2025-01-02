04d385d9ecdab483e08cfae7f7422c34c5e9b501
josefortyfive <jose.fortyfive@gmail.com>
2025-01-02 13:47:17 +0800

    Created new Porject and a new class Course.java

---
+++ 09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-many/cruddemo/src/main/java/com/learning/cruddemo/CruddemoApplication.java
@@ -0,0 +1,104 @@
+package com.learning.cruddemo;
+
+import com.learning.cruddemo.dao.AppDAO;
+import com.learning.cruddemo.entity.Instructor;
+import com.learning.cruddemo.entity.InstructorDetail;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CruddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruddemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(AppDAO appDAO){
+		return runner -> {
+			// createInstructor(appDAO);
+			// findInstructor(appDAO);
+			// deleteInstructor(appDAO);
+			// findInstructorDetail(appDAO);
+			deleteInstructorDetail(appDAO);
+		};
+	}
+
+	private void deleteInstructorDetail(AppDAO appDAO) {
+
+		int theId = 3;
+		System.out.println("Deleting instructor detail id: "+theId);
+
+		appDAO.deleteInstructorDetailById(theId);
+
+		System.out.println("Done ..");
+	}
+
+	private void findInstructorDetail(AppDAO appDAO) {
+		// get the instructor detail object
+		int theId = 2;
+		InstructorDetail instructorDetail = appDAO.findInstructorDetailById(theId);
+		// print the instructor detail
+
+		System.out.println("Instructor Detail: "+instructorDetail);
+
+		// print the associated instructor
+		System.out.println("Associated instructor: "+instructorDetail.getInstructor());
+
+		System.out.println("Done..");
+
+	}
+
+	private void deleteInstructor(AppDAO appDAO) {
+		int theId = 1;
+		System.out.println("Deleting instructor..." + theId);
+
+		appDAO.deleteInstructorById(theId);
+
+		System.out.println("Done deleting instructor.");
+	}
+
+	private void findInstructor(AppDAO appDAO) {
+		int theId = 2;
+		System.out.println("Find instructor id: "+ theId);
+
+		Instructor tempInstructor = appDAO.findInstructorById(theId);
+
+		System.out.println("Instructor: "+tempInstructor);
+		System.out.println("the associate instructorDetail only: "+tempInstructor.getInstructorDetail());
+
+	}
+
+
+	private void createInstructor(AppDAO appDAO) {
+
+
+		// create the instructor
+		Instructor tempInstructor = new Instructor("Chad", "Darby", "darby@luv2code.com");
+
+		// create the instructor detail
+		InstructorDetail tempInstructorDetail = new InstructorDetail("http://www.luv2code.com/youtube", "Luve 2 code!!!");
+
+		// create the instructor
+		// Instructor tempInstructor = new Instructor("Edmar", "Canin", "edmar@java.com");
+
+		// create the instructor detail
+		// InstructorDetail tempInstructorDetail = new InstructorDetail("http://www.codejava.com/youtube", "Coding!!!");
+
+		// associate the objects
+		tempInstructor.setInstructorDetail(tempInstructorDetail);
+
+		// save the instructor
+		//
+		// NOTE: This will ALSO save the details object
+		// because of CascadeType.ALL
+		//
+		System.out.println("Saving instructor: "+tempInstructor);
+		appDAO.save(tempInstructor);
+
+		System.out.println("Done!");
+	}
+}
---
+++ 09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-many/cruddemo/src/main/java/com/learning/cruddemo/dao/AppDAO.java
@@ -0,0 +1,17 @@
+package com.learning.cruddemo.dao;
+
+import com.learning.cruddemo.entity.Instructor;
+import com.learning.cruddemo.entity.InstructorDetail;
+
+public interface AppDAO {
+
+    void save(Instructor theInstructor);
+
+    Instructor findInstructorById(int theId);
+
+    void deleteInstructorById(int theId);
+
+    InstructorDetail findInstructorDetailById(int theId);
+
+    void deleteInstructorDetailById(int theId);
+}
