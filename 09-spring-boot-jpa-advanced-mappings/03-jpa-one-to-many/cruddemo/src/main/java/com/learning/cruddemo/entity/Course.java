c64ec79e2b1952b4b406000958878ac648f234c4
josefortyfive <jose.fortyfive@gmail.com>
2025-01-02 13:51:24 +0800

    Added Course Instructor variable @ManyToOne Annotation

--- 09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-many/cruddemo/src/main/java/com/learning/cruddemo/entity/Course.java
+++ 09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-many/cruddemo/src/main/java/com/learning/cruddemo/entity/Course.java
@@ -1,14 +1,21 @@
 package com.learning.cruddemo.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "course")
 public class Course {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
     private int id;
+
+    @Column(name="title")
     private String title;
+
+    @ManyToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinColumn(name="instructor_id")
     private Instructor instructor;
 
     public Course(){
